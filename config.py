7c89cb0f6c7237c5eab309b87b49f6a708472a35
GuNeR <guner@ravnovesie.net>
2024-01-31 22:53:19 +0800

    Thread improvements.

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WEIGHTS_PER_PAGE = 20
+    WEIGHTS_PER_PAGE = 10
     # SCALES_HOST = '192.168.6.18'
     # SCALES_PORT = 11001
     SCALES_HOST = '192.168.2.70'
