d7696632f03d86176008bae9c729ac32a1a5bc4f
GuNeR <guner@ravnovesie.net>
2024-02-05 18:56:13 +0800

    Client java script. Socket disconnection info added.

--- app/static/js/application.js
+++ app/static/js/application.js
@@ -7,4 +7,16 @@ $(document).ready(function(){
     socket.on('weight', function(msg) {
         $('#weight').html(msg.data);
     });
+
+    // handle socket disconnect event
+    socket.on('disconnect', function(){
+        console.log('Socket connection closed.');
+        $('#weight').html("Connecting to scales...");
+    });
+
+    // handle socket errors
+    socket.on('error', function(error){
+        console.error('Socket error:', error);
+        $('#weight').html("Connecting to scales...");
+    });
 });
